f1677f19e4da0aee3eb87ecd11d2c0c2a4adce8f
Bongienm <bongigugu@gmail.com>
2022-10-16 10:26:43 +0200

    No error

--- app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
@@ -9,17 +9,28 @@ import com.example.a7minuteworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        handleStartExercise()
+    }
 
-//        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        binding?.flStart?.setOnClickListener{
-            val intent = Intent(this,ExerciseActivity::class.java)
+    private fun handleStartExercise() {
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener{
+            val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
         }
 
+        binding?.apply {
+            flStart.setOnClickListener {
+//                Toast.makeText(this@MainActivity, "Start Exercise", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, ExerciseActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onDestroy() {
